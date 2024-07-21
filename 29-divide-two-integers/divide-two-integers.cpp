0e8fd75498db195db32adfe256b5eda0a574e7d2
Divyank Sharma <97398369+davesharmax@users.noreply.github.com>
2024-07-21 16:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (6.48%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -5,10 +5,10 @@ public:
         if( dividend == INT_MIN && divisor == -1 )
             return INT_MAX;
         long long int ans = dividend/divisor;
-        if(ans>INT_MAX)
-            return INT_MAX;
-        if(ans<INT_MIN)
-            return INT_MIN;
+        // if(ans>INT_MAX)
+        //     return INT_MAX;
+        // if(ans<INT_MIN)
+        //     return INT_MIN;
         return ans;
     }
 };
